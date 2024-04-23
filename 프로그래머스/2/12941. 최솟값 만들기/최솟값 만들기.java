a94ff450232cf6e18400b3cd46f5cd24fbbb2db6
김상엽 <54929479+KIM-TABLE-NEXT@users.noreply.github.com>
2024-04-23 18:33:56 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.06 ms, Memory: 52.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []a, int []b)
+    {
+        int answer = 0;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for(int i=0; i<a.length;i++)
+            answer += a[i]*b[a.length-1-i];
+
+        return answer;
+    }
+}
